7aeb16d4d9d7fc3b1c54e7ef80b394fe0c0042ef
jholmes847 <93491719+jholmes847@users.noreply.github.com>
2022-05-17 09:59:35 -0700

    commit main

--- server.js
+++ server.js
@@ -55,14 +55,17 @@ app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
 // Routes
 
 ///Edit
-app.get('/drinks/edit', (req, res)=>{
-  res.render('edit.ejs');
-});
+app.get('/drinks/edit',(req,res) => {
+  Drink.findById(req.params.id, (error,foundDrink) => {
+  res.render('edit.ejs',{
+    drinks: foundDrink,
+  })
+})
+})
 
-
-app.put('/:id', (req,res) => {
-  Drink.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedDrink) => {
-    res.redirect('/')
+app.put('/drinks/:id/', (req,res) => {
+  Drink.findByIdAndUpdate(req.params.id, req.body,{new:true},(error,foundDrink) => {
+    res.redirect('/drinks')
   })
 })
 //Index
